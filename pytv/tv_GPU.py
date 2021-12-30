55e8d2a21c09af769da18dcc502a6ec244efc14b
eboigne <eboigne@stanford.edu>
2021-12-29 17:42:19 -0800

    Added copyright header

--- pytv/tv_GPU.py
+++ pytv/tv_GPU.py
@@ -1,3 +1,46 @@
+# /*-----------------------------------------------------------------------*\
+# |                                                                         |
+# |                 _____            _______  __      __                    |
+# |                |  __ \          |__   __| \ \    / /                    |
+# |                | |__) |  _   _     | |     \ \  / /                     |
+# |                |  ___/  | | | |    | |      \ \/ /                      |
+# |                | |      | |_| |    | |       \  /                       |
+# |                |_|       \__/ |    |_|        \/                        |
+# |                           __/ |                                         |
+# |                          |___/                                          |
+# |                                                                         |
+# |                                                                         |
+# |   Author: E. Boigne                                                     |
+# |                                                                         |
+# |   Contact: Emeric Boigne                                                |
+# |   email: emericboigne@gmail.com                                         |
+# |   Department of Mechanical Engineering                                  |
+# |   Stanford University                                                   |
+# |   488 Escondido Mall, Stanford, CA 94305, USA                           |
+# |                                                                         |
+# |-------------------------------------------------------------------------|
+# |                                                                         |
+# |   This file is part of the pyTV package.                                |
+# |                                                                         |
+# |   License                                                               |
+# |                                                                         |
+# |   Copyright(C) 2021 E. Boigne                                           |
+# |   pyTV is free software: you can redistribute it and/or modify          |
+# |   it under the terms of the GNU General Public License as published by  |
+# |   the Free Software Foundation, either version 3 of the License, or     |
+# |   (at your option) any later version.                                   |
+# |                                                                         |
+# |   pyTV is distributed in the hope that it will be useful,               |
+# |   but WITHOUT ANY WARRANTY; without even the implied warranty of        |
+# |   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         |
+# |   GNU General Public License for more details.                          |
+# |                                                                         |
+# |   You should have received a copy of the GNU General Public License     |
+# |   along with pyTV. If not, see <http://www.gnu.org/licenses/>.          |
+# |                                                                         |
+# /*-----------------------------------------------------------------------*/
+
+
 import numpy as np
 import torch
 import pytv.tv_2d_GPU
